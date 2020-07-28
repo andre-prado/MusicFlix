194b582c0693e7046a76ce72c6b86bb29b542b0f
andre-prado <andr3prad0@gmail.com>
2020-07-28 16:54:46 -0300

    carousel added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria/index';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+      <Route component={() => (<h1>Página não encontrada ):</h1>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
