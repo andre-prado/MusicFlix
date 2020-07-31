c62e9a846cf7b2b0cf779c587946496a921873f3
andre-prado <andr3prad0@gmail.com>
2020-07-31 14:20:59 -0300

    infinity carousel

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -43,9 +43,8 @@ const Slider = ({ children }) => (
   <Container>
     <SlickSlider {...{
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 300,
-      centerMode: false,
       variableWidth: true,
       adaptiveHeight: true,
     }}
